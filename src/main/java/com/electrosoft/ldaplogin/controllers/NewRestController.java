19d147173a3b0ec2a0be9dc530d485f5e85b95a7
hdavella <hdavella@gmail.com>
2024-07-31 14:15:00 -0300

    new controller

---
+++ src/main/java/com/electrosoft/ldaplogin/controllers/NewRestController.java
@@ -0,0 +1,59 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/SpringFramework/RestController.java to edit this template
+ */
+package com.electrosoft.ldaplogin.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.List;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+/**
+ *
+ * @author hdavella
+ */
+@RestController
+@RequestMapping("/url")
+public class NewRestController {
+    
+    @GetMapping()
+    public List<Object> list() {
+        return null;
+    }
+    
+    @GetMapping("/{id}")
+    public Object get(@PathVariable String id) {
+        return id.toString();
+    }
+    
+    @PutMapping("/{id}")
+    public ResponseEntity<?> put(@PathVariable String id, @RequestBody Object input) {
+        return null;
+    }
+    
+    @PostMapping
+    public ResponseEntity<?> post(@RequestBody Object input) {
+        return null;
+    }
+    
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable String id) {
+        return null;
+    }
+    
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR, reason = "Error message")
+    public void handleError() {
+    }
+    
+}
